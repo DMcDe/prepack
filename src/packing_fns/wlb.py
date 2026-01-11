6adfb9d83a15150b57eef1bb2eec5b8c48c109b5
DMcDe <dmcde@umich.edu>
2026-01-11 08:37:42 -0500

    finish transferring wlb packer

--- src/packing_fns/wlb.py
+++ src/packing_fns/wlb.py
@@ -1,3 +1,6 @@
+from prepack.packer import OfflinePacker
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
 from typing import List
 
 class OutlierQueue:
@@ -102,3 +105,80 @@ class Microbatch:
         """
         return Microbatch.attention_workload(length) + Microbatch.operation_workload(length)
     
+class WLBOfflinePacker(OfflinePacker):
+    def __init__(self, json_path: str, data_col: str, output_csv: str, tokenizer: AutoTokenizer, queue_bounds: List[int]):
+        super().__init__(json_path, data_col, output_csv, tokenizer)
+        self.packed_microbatches = []
+        self.queue = OutlierQueue(queue_bounds)
+        self.file_assignments = {}
+
+    def iteration(self, docs: List[int], num_microbatches: int, max_size: int, remained_docs: List[int], iter_number: int) -> List[int]:
+        doc_lens = self.get_doc_lens(docs)
+
+        new_docs = []
+
+        # Push outlier documents to their respective queue, and add to new_docs otherwise
+        for fname, flen in doc_lens.items():
+            if not self.queue.check_outlier(fname, flen):
+                new_docs.append((flen, fname))
+
+        # Pop N outlier documents for any queue that has at least one per patch
+        for q in self.queue.outlier_queues:
+            if len(q) >= num_microbatches:
+                for _ in range(num_microbatches):
+                    new_docs.append(q.pop())
+
+        # Sort in descending order by length
+        new_docs.sort(reverse=True)
+
+        # TODO: Can this be done faster with some method other than prepending?
+        all_docs = remained_docs + new_docs
+        remained_docs.clear()
+
+        new_batches = [Microbatch(i) for i in range(num_microbatches)]
+
+        for doc in all_docs:
+            # Get microbatch w min workload and min length
+            w_min = min(new_batches, key=lambda mb: mb.workload)
+            l_min = min(new_batches, key=lambda mb: mb.tokens)
+
+            # Try to add to lowest workload microbatch first
+            if w_min.tokens + doc[0] <= max_size:
+                w_min.push_document(doc[0], Microbatch.estimate_workload(doc[0]))
+                self.file_assignments[doc[1]] = w_min.index + iter_number * num_microbatches
+
+            # If no space, try to add to lowest length microbatch
+            elif l_min.tokens + doc[0] <= max_size:
+                l_min.push_document(doc[0], Microbatch.estimate_workload(doc[0]))
+                self.file_assignments[doc[1]] = l_min.index + iter_number * num_microbatches
+
+            # If neither has space, delay packing until next iteration
+            else:
+                remained_docs.append(doc)
+
+        return remained_docs
+    
+    def pack_microbatches(self, **kwargs):
+        num_microbatches: int = kwargs["num_microbatches"]
+        max_size: int = kwargs["max_size"]
+        num_iterations: int = kwargs["num_iterations"]
+
+        if num_microbatches is None or max_size is None or num_iterations is None:
+            raise KeyError("Required kwarg not given. Pack_microbatches must specify num_microbatches, max_size, and num_iterations.")
+        
+        dataloader = DataLoader(self.dataset, batch_size=(len(self.dataset) + num_iterations - 1) // num_iterations, 
+                                shuffle=True, collate_fn=OfflinePacker.varlen_collate)
+        data_iterator = iter(dataloader)
+
+        # Loop through batches in dataloader
+        remained_docs = []
+        for i in range(num_iterations):
+            # Iterate until end, return empty list if we hit end
+            fileset = next(data_iterator, [])
+            if fileset == []:
+                break
+
+            remained_docs = self.iteration(fileset, num_microbatches, max_size, remained_docs, i)
+
+        return self.dict_to_list(self.file_assignments)
+        
