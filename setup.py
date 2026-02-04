da0e13fa977a94bbdb20753402e08ab42d35c810
DMcDe <dmcde@umich.edu>
2026-02-04 09:04:54 -0500

    turn into installable package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="prepack",
+      version="0.3",
+      description="Efficient offline microbatch packer for AI training (WIP)",
+      url="https://github.com/DMcDe/prepack",
+      author="David McDermott",
+      packages=["prepack"])
